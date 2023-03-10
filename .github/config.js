0fa437d9b7cae4140f6e414e64c1e9d59410caaa
Aherkar <amol.aherkar@capgemini.com>
2023-03-10 11:49:12 +0530

    test

---
+++ .github/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    branchPrefix: 'test-renovate/',
+    gitAuthor: 'Amol Aherkar <amolaherkar@gmail.com>',
+    onboarding: false,
+    platform: 'github',
+    repositories: ['amolaherkar/Test-GitHubActions'],
+};
