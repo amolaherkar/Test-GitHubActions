f8ac39607dac6f36c20a840e06fd147f9e702db9
amolaherkar <40294149+amolaherkar@users.noreply.github.com>
2023-03-10 13:46:14 +0530

    Update config.js

--- .github/config.js
+++ .github/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    token: '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',
     branchPrefix: 'test-renovate/',
     gitAuthor: 'Amol Aherkar <amolaherkar@gmail.com>',
     onboarding: false,
